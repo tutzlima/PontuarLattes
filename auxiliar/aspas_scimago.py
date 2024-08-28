0f122856d0459996d386ca716e262575aaee59fe
tutzlima <arthurlimareserva@gmail.com>
2024-08-28 13:29:00 -0300

    Removendo arquivo desnecessário

--- auxiliar/aspas_scimago.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("scimagojr 2019.csv", mode="r") as infile:
-    reader = csv.reader(infile, delimiter=";")    
-    with open("/tmp/scimagojrTAB", mode="w") as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-        writer.writerows(reader)
-
